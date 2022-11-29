6359bd11347ad784c68135c69d5d593c774aa4ba
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2022-11-29 23:57:22 +0530

    home content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading,Text  } from "@chakra-ui/react";
+import { Box, Heading,Text,AspectRatio} from "@chakra-ui/react";
 import Head from "next/head";
 import BaseLayout from "../components/BaseLayout";
 
@@ -13,8 +13,19 @@ export default function Home() {
       <main>
         <BaseLayout>
           {/* write from here */}
-          <Heading>I&apos; m a Heading</Heading>
-          <Text fontSize=''>(3xl) In love with React & Next</Text>
+          <Heading>Come and join us</Heading>
+          // This video will have equal sides
+<AspectRatio maxW='1000px' ratio={2}  pl={6} >
+  <iframe
+    title='naruto'
+    src='https://youtu.be/Iwa2dpEYyPk?list=PLmkIPPvcHLZgWaBGqwiAe52PLDNnMtIA5'
+    allowFullScreen
+  />
+</AspectRatio>
+          <Text fontSize='2xl' pt={5}>The safety and well-being of all conference participants is our priority. After evaluating the current COVID-19 situation in New Zealand and the uncertainty around International travel, the decision has been made to transform the in-person component of IEEE VR 2022 into an all-digital conference experience and will now be an online event. Therefore, IEEE VR 2022 will no longer take place in Christchurch, New Zealand and will instead take place virtually. The conference dates remain the same (12 – 16 March 2022). Proceedings will not be cancelled, and publications will continue as planned.</Text>
+          <Text fontSize='2xl' pt={5}>Though the decision has been made to transform IEEE VR 2022 into an all-digital online conference experience, several members of the IEEE VR community around the world have agreed to organise small meeting hubs for those able to travel locally. Please visit the satellite events page to find an event near you.</Text>
+          <Text fontSize='2xl' pt={5}>Our goal is to provide the best experience for virtual attendees, following in the historical footsteps of the virtual experience of VR 2020 in Atlanta and VR 2021 in Lisbon.</Text>
+          <Text fontSize='2xl' pt={5}>Come join us for an unforgettable experience and explore the virtuality continuum in the premier event covering extended reality featuring oral presentations, posters, research demos, tutorials, and workshops.</Text>
         </BaseLayout>
       </main>
     </div>
