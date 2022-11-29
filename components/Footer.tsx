4a3f221950277cd4d8d9e3fe82f0f4760e6fa0d5
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2022-11-29 23:27:06 +0530

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,9 @@
-import { Box, Center,Text,Link,Divider } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 
 function Footer() {
   return (
     <Box>
-      <Center pt='150px'>
-      <Divider  pt={10} />
-      </Center>
-      <Center bg="white.200"  position="relative" bottom="0" pt={1} >
-         <Link pt={5}><Text as='u' color='blue' fontSize='xs'>Code of Conduct</Text></Link>
-         
-      </Center>
-      <Center>
-      <Text>© IEEEVR Conference</Text>
-      </Center>
+      <Center bg="red.200"  position="relative" bottom="0">Footer</Center>
     </Box>
   );
 }
