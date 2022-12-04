8a11603bd95a75072ed406002be530edf9c3f169
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2022-12-05 00:15:25 +0530

    committee page update

--- pages/committee/conferenceCommittee.tsx
+++ pages/committee/conferenceCommittee.tsx
@@ -1,10 +1,76 @@
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from '@chakra-ui/react'
+import { Box, Button, Heading, Text, Input,InputGroup, ButtonGroup,InputRightElement,Divider } from "@chakra-ui/react";
 import BaseLayout from "../../components/BaseLayout"
 
 function ConferenceCommittee() {
   return (
     <BaseLayout>
     {/* write from here */}
+    <Heading as='h3' size='lg'>
+    Patron
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Dr. Prashanta Kumar Patra , OSD, OUTR, Bhubaneswar</ListItem>
+</UnorderedList>
+  </Box>
+  <Heading as='h3' size='lg'>
+  Convener
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Dr.Jibitesh Mishra, Professor & HOD, Department of CSA, OUTR, Bhubaneswar</ListItem>
+</UnorderedList>
+  </Box>
+  <Heading as='h3' size='lg'>
+  Organising Chair
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Dr.Jibitesh Mishra, Professor & HOD, Department of CSA, OUTR, Bhubaneswar</ListItem>
+  <ListItem>Dr.Ranjan Kumar Dash, Professor & HOD, Department of IT, OUTR, Bhubaneswar</ListItem>
+  <ListItem>Prof. Tapas Kumar Patra , Dept. of E&IE , OUTR,Bhubanewar</ListItem>
+</UnorderedList>
+  </Box>
+  <Heading as='h3' size='lg'>
+  Finance Chair
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Prof. Tapas Kumar Patra , Dept. of E&IE , OUTR,Bhubanewa</ListItem>
+</UnorderedList>
+  </Box>
+  <Heading as='h3' size='lg'>
+  General Chair
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Prof. Bijaya Ketan Panigrahi, IIT, Delhi</ListItem>
+</UnorderedList>
+  </Box>
+  <Heading as='h3' size='lg'>
+  Publicity Chair
+  </Heading>
+  <Box w='50%' h='5px' bgGradient='linear(to-r, pink.500, orange.500)' />
+  <Box>
+  <UnorderedList>
+  <ListItem>Prof. Bijaya Ketan Panigrahi, IIT,Delhi</ListItem>
+  <ListItem>Dr.Subhasis Mahapatra, HOD, Department of CSE, OUTR, Bhubaneswar</ListItem>
+</UnorderedList>
+  </Box>
     </BaseLayout>
+    
   )
 }
 export default ConferenceCommittee
--- pages/committee/programCommittee.tsx
+++ pages/committee/programCommittee.tsx
@@ -1,9 +1,115 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Input,
+  InputGroup,
+  ButtonGroup,
+  InputRightElement,
+  Divider,
+} from "@chakra-ui/react";
 import BaseLayout from "../../components/BaseLayout"
 
 function ProgramCommittee() {
   return (
     <BaseLayout>
     {/* write from here */}
+    <Heading as="h3" size="lg">
+    Program Committee
+      </Heading>
+      <Box w="50%" h="5px" bgGradient="linear(to-r, pink.500, orange.500)" />
+      <TableContainer>
+        <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>SL.NO</Th>
+            <Th>NAME</Th>
+            <Th>UNIVERSITY/COLLAGE</Th>
+            <Th>PLACE</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>1</Td>
+            <Td>Prof. Poonam Singh</Td>
+            <Td>NIT</Td>
+            <Td>Rourkela</Td>
+          </Tr>
+          <Tr>
+            <Td>2</Td>
+            <Td>Prof. Saraju P. Mohanty</Td>
+            <Td>UNT</Td>
+            <Td>Denton</Td>
+          </Tr>
+          <Tr>
+            <Td>3</Td>
+            <Td>Prof. Sriram Chellappan</Td>
+            <Td>USF</Td>
+            <Td>Florida</Td>
+          </Tr>
+          <Tr>
+            <Td>4</Td>
+            <Td>Prof. Sarojananda Mishra</Td>
+            <Td>IGIT</Td>
+            <Td>Sarang</Td>
+          </Tr>
+          <Tr>
+            <Td>5</Td>
+            <Td>Prof. M S Bhatt</Td>
+            <Td>NIT</Td>
+            <Td>Surtkal</Td>
+          </Tr>
+          <Tr>
+            <Td>6</Td>
+            <Td>Dr. Albert Sunny</Td>
+            <Td>IIT</Td>
+            <Td>Palakkad</Td>
+          </Tr>
+          <Tr>
+            <Td>7</Td>
+            <Td>Dr. Niladri Bihari Puhan</Td>
+            <Td>IIT</Td>
+            <Td>Bhubaneswar</Td>
+          </Tr>
+          <Tr>
+            <Td>8</Td>
+            <Td>Dr. Manoranjan Satpathy</Td>
+            <Td>IIT</Td>
+            <Td>Bhubaneswar</Td>
+          </Tr>
+          <Tr>
+            <Td>9</Td>
+            <Td>Dr. Barathram. Ramkumar</Td>
+            <Td>IIT</Td>
+            <Td>Bhubaneswar</Td>
+          </Tr>
+          <Tr>
+            <Td>10</Td>
+            <Td>Dr. Soumya Prakash Dash</Td>
+            <Td>IIT</Td>
+            <Td>Bhubaneswar</Td>
+          </Tr>
+          <Tr>
+            <Td>11</Td>
+            <Td>Prof. Debasish Jena</Td>
+            <Td>IIT</Td>
+            <Td>Bhubaneswar</Td>
+          </Tr>
+        </Tbody>
+        </Table>
+      </TableContainer>
     </BaseLayout>
   )
 }
