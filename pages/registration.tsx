c525f779f627da15c49c8de0a6b7592f3a27b3e3
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2022-12-04 22:54:33 +0530

    registration page update

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,9 +1,49 @@
 import BaseLayout from "../components/BaseLayout"
+import {
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
 function Registration() {
   return (
     <BaseLayout>
     {/* write from here */}
+    <Box pt={10}>
+    <Table border='1px' >
+      <Thead border='1px' >
+        <Tr>
+          <Th>PARTICIPANT CATEGORY</Th>
+          <Th>NATIONAL</Th>
+          <Th>OVERSEAS (USD)</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+      <Tr>
+        <Td>Industrial Delegates</Td>
+        <Td>6,000 INR</Td>
+        <Td >80 USD</Td>
+      </Tr>
+      <Tr>
+        <Td>Academic Delegates</Td>
+        <Td>5,000 INR</Td>
+        <Td >65 USD</Td>
+      </Tr>
+      <Tr>
+        <Td>Students (U.G., P.G., Ph.D., Research Scholars)</Td>
+        <Td>4,000 INR</Td>
+        <Td >55 USD</Td>
+      </Tr>
+    </Tbody>
+    </Table>
+    </Box>
     </BaseLayout>
   )
 }
