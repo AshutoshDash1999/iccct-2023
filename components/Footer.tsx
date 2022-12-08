8102c9271e12af4a23402f16fb10b4b98cdd2145
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2022-12-09 01:29:01 +0530

    footer page update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,47 @@
-import { Box, Center, Text, Link, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Text,
+  Link,
+  Divider,
+  Flex,
+  Spacer,
+  Heading,
+  Stack,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
 
 function Footer() {
   return (
     <Box>
+      <Flex p="4">
+        <Box>
+          <Heading as="h2" size="sm">
+            ICCCT-2023
+          </Heading>
+          <Text>Odisha University of Technology and Research (OUTR)</Text>
+          <Text>All Rights Reserved</Text>
+        </Box>
+        <Spacer />
+        <Box>
+          <VStack>
+            <Heading as="h4" size="sm">
+              Get in Touch
+            </Heading>
+            <Text>Odisha University of Technology and Research (OUTR)</Text>
+            <Text>All Rights Reserved</Text>
+            <Text>085-132567</Text>
+            <Text>csa@outr.net</Text>
+          </VStack>
+        </Box>
+      </Flex>
+
       <Center>
-        <Divider py={10} />
-      </Center>
-      <Center bg="white.200" pt={1}>
-        <Link pt={5}>
-          <Text as="u" color="blue" fontSize="xs">
-            Code of Conduct
-          </Text>
-        </Link>
-      </Center>
-      <Center>
-        <Text>© ICCCT 2023</Text>
+        <VStack>
+          <Text>Odisha University of Technology and Research (OUTR)</Text>
+          <Text>All Rights Reserved</Text>
+        </VStack>
       </Center>
     </Box>
   );
